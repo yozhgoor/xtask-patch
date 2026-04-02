99fb0339c72db80084ddd10d603c849e2c9f3614
yozhgoor <yozhgoor@outlook.com>
2026-04-03 00:11:04 +0200

    Clean up

--- examples/demo/itertools/src/lib.rs
+++ examples/demo/itertools/src/lib.rs
@@ -1,3 +1,14 @@
-pub fn join(_collection: impl IntoIterator, _sep: &str) -> String {
-    todo!()
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
 }
